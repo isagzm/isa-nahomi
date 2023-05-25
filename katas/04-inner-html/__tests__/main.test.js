c982fec71e67f87f68be04ebc3536835c44402fa
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-25 19:35:35 +0100

    add prettier

--- katas/04-inner-html/__tests__/main.test.js
+++ katas/04-inner-html/__tests__/main.test.js
@@ -1,5 +1,6 @@
 import { getByText, prettyDOM } from "@testing-library/dom";
 import { readRelativeIndex } from "../../../__test__/utils.js";
+import prettier from "prettier";
 
 import { kata } from "../kata.js";
 
@@ -8,6 +9,9 @@ const html = readRelativeIndex(__dirname);
 test("structure is ok", async () => {
   document.body.innerHTML = html;
   kata();
+  document.body.innerHTML = prettier.format(document.body.innerHTML, {
+    parser: "html",
+  });
 
   expect(document.body).toMatchSnapshot();
 });
